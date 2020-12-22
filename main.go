9c0c967f5e7465bde7d068d889715ed670e1a880
Tleu13 <tleuzhan13@gmail.com>
2020-12-22 11:35:44 +0600

    basic handler example

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
-func main()  {
-	fmt.Println("Hello")
+// Handler
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello SE"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+
+	w.Write([]byte("Create a new snippet..."))
+}
+
+func main() {
+	// Router
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		panic(err)
+	}
 }
